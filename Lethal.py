2919d8005d859eed6ff247dee438ed6a765e365a
dangerousjatt <73658726+dangerousjatt@users.noreply.github.com>
2021-07-09 12:50:52 +0530

    Update Lethal.py

--- Lethal.py
+++ Lethal.py
@@ -782,6 +782,17 @@ async def help(e):
 # --------------------------------------------------------------------------------------------------------------------------------
 
 
+from telethon.tl.types import Channel, Chat, User
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import  CheckChatInviteRequest, GetFullChatRequest
+from telethon.errors import (ChannelInvalidError, ChannelPrivateError, ChannelPublicGroupNaError, InviteHashEmptyError, InviteHashExpiredError, InviteHashInvalidError)
+from telethon.tl.functions.channels import GetFullChannelRequest, GetParticipantsRequest
+
+
+
+
+
+
 async def get_chatinfo(event):
     chat = event.pattern_match.group(1)
     chat_info = None
@@ -799,34 +810,40 @@ async def get_chatinfo(event):
             chat = event.chat_id
     try:
         chat_info = await event.client(GetFullChatRequest(chat))
-    except BaseException:
+    except:
         try:
             chat_info = await event.client(GetFullChannelRequest(chat))
         except ChannelInvalidError:
             await event.reply("`Invalid channel/group`")
             return None
         except ChannelPrivateError:
-            await event.reply(
-                "`This is a private channel/group or I am banned from there`"
-            )
+            await event.reply("`This is a private channel/group or I am banned from there`")
             return None
         except ChannelPublicGroupNaError:
             await event.reply("`Channel or supergroup doesn't exist`")
             return None
-        except (TypeError, ValueError):
+        except (TypeError, ValueError) as err:
             await event.reply("`Invalid channel/group`")
             return None
     return chat_info
 
 
-async def edit_or_reply(event, msg):
-    await event.edit(msg)
+def make_mention(user):
+    if user.username:
+        return f"@{user.username}"
+    else:
+        return inline_mention(user)
+
+
+def inline_mention(user):
+    full_name = user_full_name(user) or "No Name"
+    return f"[{full_name}](tg://user?id={user.id})"
 
 
 def user_full_name(user):
     names = [user.first_name, user.last_name]
     names = [i for i in list(names) if i]
-    full_name = " ".join(names)
+    full_name = ' '.join(names)
     return full_name
 
 
@@ -840,41 +857,27 @@ def user_full_name(user):
 @cdk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
 @edk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
 @ddk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-async def get_users(event):
-    sender = await event.get_sender()
-    me = await event.client.get_me()
+sender = await event.get_sender() ; me = await event.client.get_me()
     if not sender.id == me.id:
-        hell = await event.reply("`processing...`")
+        rkp = await event.reply("`processing...`")
     else:
-        hell = await event.edit("`processing...`")
-    legendx22 = await get_chatinfo(event)
-    chat = await event.get_chat()
+    	rkp = await event.edit("`processing...`")
+    rk1 = await get_chatinfo(event) ; chat = await event.get_chat()
     if event.is_private:
-        return await hell.edit("`Sorry, Can add users here`")
-    s = 0
-    f = 0
-    error = "None"
-
-    await hell.edit("**TerminalStatus**\n\n`Collecting Users.......`")
-    async for user in event.client.iter_participants(legendx22.full_chat.id):
-        try:
-            if error.startswith("Too"):
-                return await hell.edit(
-                    f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
-                )
-            await event.client(
-                functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
-            )
-            s = s + 1
-            await hell.edit(
-                f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
-            )
-        except Exception as e:
-            error = str(e)
-            f = f + 1
-    return await hell.edit(
-        f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
-    )
+              return await rkp.edit("`Sorry, Can add users here`")    
+    s = 0 ; f = 0 ; error = 'None'   
+  
+    await rkp.edit("**TerminalStatus**\n\n`Collecting Users.......`")
+    async for user in event.client.iter_participants(rk1.full_chat.id):
+                try:
+                    if error.startswith("Too"):
+                        return await rkp.edit(f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people")
+                    await event.client(functions.channels.InviteToChannelRequest(channel=chat,users=[user.id]))
+                    s = s + 1                                                    
+                    await rkp.edit(f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`")                
+                except Exception as e:
+                    error = str(e) ; f = f + 1             
+    return await rkp.edit(f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people")
 
 
 text = """
