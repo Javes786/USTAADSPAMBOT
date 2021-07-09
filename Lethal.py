244b3f5afacdf68f9ba23053c7e32642bc5f1536
dangerousjatt <73658726+dangerousjatt@users.noreply.github.com>
2021-07-09 17:27:41 +0530

    Update Lethal.py

--- Lethal.py
+++ Lethal.py
@@ -830,53 +830,6 @@ def user_full_name(user):
     return full_name
 
 
-@idk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@ydk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@wdk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@hdk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@sdk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@adk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@bdk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@cdk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@edk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-@ddk.on(events.NewMessage(incoming=True, pattern=r"\.add"))
-async def get_users(event):
-    sender = await event.get_sender()
-    me = await event.client.get_me()
-    if not sender.id == me.id:
-        hell = await event.reply("`processing...`")
-    else:
-        hell = await event.edit("`processing...`")
-    legendx22 = await get_chatinfo(event)
-    chat = await event.get_chat()
-    if event.is_private:
-        return await hell.edit("`Sorry, Can add users here`")
-    s = 0
-    f = 0
-    error = "None"
-
-    await hell.edit("**TerminalStatus**\n\n`Collecting Users.......`")
-    async for user in event.client.iter_participants(legendx22.full_chat.id):
-        try:
-            if error.startswith("Too"):
-                return await hell.edit(
-                    f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people"
-                )
-            await event.client(
-                functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
-            )
-            s = s + 1
-            await hell.edit(
-                f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
-            )
-        except Exception as e:
-            error = str(e)
-            f = f + 1
-    return await hell.edit(
-        f"**Terminal Finished** \n\n• Successfully Invited `{s}` people \n• failed to invite `{f}` people"
-    )
-
-
 text = """
 ░░╚██╔╝░░██║░░░██║██╔═██╗░██╔═██╗░██║
 ░░░██║░░░╚██████╔╝██║░╚██╗██║░╚██╗██║
